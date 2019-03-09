0faaa440eeb7ea0311c3fa81e635a71235da128d
aien0303 <my970504@gmail.com>
2019-03-09 11:21:05 +0800

    3/9 1120 update

---
+++ crawl/Crawl.py
@@ -0,0 +1,89 @@
+import requests
+from bs4 import BeautifulSoup
+import time
+import pymysql
+import json
+
+# 爬蟲
+def crawl(Url,Select,data=''):
+    time.sleep(2)
+    headers_data={
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    }
+    if data!='':
+        response=requests.get(Url,headers=headers_data,data=data)
+    else:
+        response=requests.get(Url,headers=headers_data)
+    soup=BeautifulSoup(response.text,'lxml')
+    items=soup.select(Select)
+    return items
+
+# 選取
+def ItemFind(Items,Tag,ClassName):
+    targetList=[]
+    for item in Items:
+        target=item.find(Tag,ClassName).get_text()
+        targetList.append(target)
+    return targetList
+
+# Json
+def JsonLoad(items,ObName):
+    items=str(items)[3:-4]
+    data=json.loads(items)
+    dlist=data[ObName]
+    return dlist
+
+# 特色擷取
+def Capture(dlist):
+    global ArrTm,DepTm
+    if dlist==[]:
+        pass
+    else:
+        if dlist[0]['ArrTm']=='':
+            pass
+        else:
+            ArrTm=int(dlist[0]['ArrTm'].replace(':',''))
+        if dlist[0]['DepTm']=='':
+            pass
+        else:
+            DepTm=int(dlist[-1]['DepTm'].replace(':',''))
+    return ArrTm,DepTm
+# 特色判斷
+# 0000-1159早回早去 1600-2359晚回晚去
+def Characteristics(dlist,ArrTm,DepTm):
+    characteristics=''
+    if len(dlist)<2:
+        if len(dlist)==2:
+            characteristics+='、直飛'
+    if ArrTm!='':
+        if ArrTm<1159:
+            characteristics+='、早去'
+        elif ArrTm>1600:
+            characteristics+='、晚去'
+    if DepTm!='':
+        if DepTm<1159:
+            characteristics+='、早回'
+        elif DepTm>1600:
+            characteristics+='、晚回'
+    if characteristics!='':
+        characteristics=characteristics[1:]
+    return characteristics
+
+# 存入資料庫
+def DBI(days,name,date,price,available,total,travelagency,characteristics=''):
+    db=pymysql.connect("localhost","root","root",'test')
+    target=[
+        days,
+        name,
+        date,
+        price,
+        available,
+        total,
+        travelagency,
+        characteristics,
+    ]
+    with db.cursor() as cursor:
+        sql="""INSERT IGNORE INTO products(days,name,date,price,available,total,travelagency,characteristics) values("""+'%s'+',%s'*(len(target)-1)+""")"""
+        cursor.execute(sql,target)
+        db.commit()
+    return print('存入資料庫')
--- crawl/readme.txt
+++ crawl/readme.txt
@@ -1,9 +1,41 @@
-apple=第一次寫的爬蟲
-# 蘋果日報
-# 標題、時間、分類
-nogizaka46=應用
-# 先放到mysql再讀出來
-# 乃木坂46
-# 歌名、歌詞、影片youtube網址
-JSc=動態網頁的爬蟲
-# 新魅力網站
+products1.py=newamazing
+products2.py=orangetour
+Crawl.py=function
+test.sql=MySQL
+
+
+SQL:
+DB=test
+# 兩個table
+
+table=product
+# Pid PK
+# days int '天'去''(乾淨)
+# name 改成非Uni
+# date date格式 '/'改'-'(格式需求)
+# price int ','去''
+# available int(乾淨)
+# total int
+# travelagency int
+# characteristics
+
+table=travelagency
+# TAid PK
+# TAname Uni
+
+
+加分項目:
+資料的正確性以及乾淨程度
+# 更改
+
+抓取兩家旅行社兩頁以上的商品資料
+# 成功
+
+多抓取步驟3. 以外，而有出現在Tripesso網站上的內容
+# 成功
+
+使用requests
+# 成功
+
+程式碼的維護性及可擴充性
+# 二次更改
